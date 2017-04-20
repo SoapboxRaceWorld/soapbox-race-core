486ba20e49963b0ee77f279d394395525489d7da
Nilzao <nilzao@gmail.com>
2017-04-19 23:47:18 -0400

    marshal with xmlns

--- src/main/java/com/soapboxrace/jaxb/util/MarshalXML.java
+++ src/main/java/com/soapboxrace/jaxb/util/MarshalXML.java
@@ -8,17 +8,24 @@ import javax.xml.bind.Marshaller;
 
 public class MarshalXML {
 
-	public static String marshal(JAXBElement<?> jaxbElement) {
+	public static String marshal(JAXBElement<?> jaxbElement, String schemaLocation) {
 		StringWriter stringWriter = new StringWriter();
 		try {
 			JAXBContext jaxbContext = JAXBContext.newInstance(jaxbElement.getValue().getClass());
 			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
 			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, false);
 			jaxbMarshaller.setProperty(Marshaller.JAXB_FRAGMENT, true);
+			if (schemaLocation != null) {
+				jaxbMarshaller.setProperty(Marshaller.JAXB_SCHEMA_LOCATION, schemaLocation);
+			}
 			jaxbMarshaller.marshal(jaxbElement, stringWriter);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return stringWriter.toString();
+	}
+
+	public static String marshal(JAXBElement<?> jaxbElement) {
+		return marshal(jaxbElement, null);
 	}
 }
