3228e06d056c0d0061464d76d4cc87f0993636ac
Nilzao <nilzao@gmail.com>
2017-04-19 23:47:37 -0400

    getFriendList

---
+++ src/main/java/com/soapboxrace/core/servlet/GetFriendListFromUserId.java
@@ -0,0 +1,32 @@
+package com.soapboxrace.core.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.xml.bind.JAXBElement;
+
+import com.soapboxrace.jaxb.http.ObjectFactory;
+import com.soapboxrace.jaxb.http.PersonaFriendsList;
+import com.soapboxrace.jaxb.util.MarshalXML;
+
+@WebServlet(urlPatterns = { "/Engine.svc/getfriendlistfromuserid" })
+public class GetFriendListFromUserId extends GenericServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2125697541381049590L;
+
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		PersonaFriendsList personaFriendsList = new PersonaFriendsList();
+		JAXBElement<PersonaFriendsList> createPersonaFriendsList = new ObjectFactory().createPersonaFriendsList(personaFriendsList);
+		String xmlns = "http://schemas.datacontract.org/2004/07/Victory.TransferObjects.DriverPerson";
+		String marshal = MarshalXML.marshal(createPersonaFriendsList, xmlns);
+		response.getOutputStream().write(marshal.getBytes());
+	}
+
+}
