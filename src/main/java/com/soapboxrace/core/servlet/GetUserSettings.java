42cee7da61317a90a77487c0a2128a859e28dcb6
Nilzao <nilzao@gmail.com>
2017-04-20 21:52:54 -0400

    creating servlets

---
+++ src/main/java/com/soapboxrace/core/servlet/GetUserSettings.java
@@ -0,0 +1,52 @@
+package com.soapboxrace.core.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.xml.bind.JAXBElement;
+
+import com.soapboxrace.jaxb.http.ArrayOfLong;
+import com.soapboxrace.jaxb.http.ArrayOfString;
+import com.soapboxrace.jaxb.http.ObjectFactory;
+import com.soapboxrace.jaxb.http.UserSettings;
+import com.soapboxrace.jaxb.util.MarshalXML;
+
+@WebServlet(urlPatterns = { "/Engine.svc/getusersettings" })
+public class GetUserSettings extends GenericServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2125697541381049590L;
+
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		UserSettings userSettings = new UserSettings();
+		userSettings.setCarCacheAgeLimit(600);
+		userSettings.setIsRaceNowEnabled(true);
+		userSettings.setMaxCarCacheSize(250);
+		userSettings.setMinRaceNowLevel(2);
+		userSettings.setVoipAvailable(false);
+		ArrayOfString arrayOfString = new ArrayOfString();
+		arrayOfString.getString().add("SCENERY_GROUP_CHRISTMAS");
+		userSettings.setActivatedHolidaySceneryGroups(arrayOfString);
+		ArrayOfLong arrayOfLong = new ArrayOfLong();
+		arrayOfLong.getLong().add(0L);
+		userSettings.setActiveHolidayIds(arrayOfLong);
+		ArrayOfString arrayOfString2 = new ArrayOfString();
+		arrayOfString2.getString().add("SCENERY_GROUP_CHRISTMAS_DISABLE");
+		userSettings.setDisactivatedHolidaySceneryGroups(arrayOfString2);
+		userSettings.setFirstTimeLogin(false);
+		userSettings.setMaxLevel(60);
+		userSettings.setStarterPackApplied(false);
+		userSettings.setUserId(getUserId(request));
+
+		JAXBElement<UserSettings> createUserSettings = new ObjectFactory().createUserSettings(userSettings);
+		String marshal = MarshalXML.marshal(createUserSettings);
+		response.getOutputStream().write(marshal.getBytes());
+	}
+
+}
---
+++ src/main/java/com/soapboxrace/core/servlet/SystemInfo.java
@@ -0,0 +1,56 @@
+package com.soapboxrace.core.servlet;
+
+import java.io.IOException;
+import java.util.GregorianCalendar;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.xml.bind.JAXBElement;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import com.soapboxrace.jaxb.http.ObjectFactory;
+import com.soapboxrace.jaxb.util.MarshalXML;
+
+@WebServlet(urlPatterns = { "/Engine.svc/systeminfo" })
+public class SystemInfo extends GenericServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -207709800210654924L;
+
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		com.soapboxrace.jaxb.http.SystemInfo systemInfo = new com.soapboxrace.jaxb.http.SystemInfo();
+		systemInfo.setBranch("production");
+		systemInfo.setChangeList("620384");
+		systemInfo.setClientVersion("637");
+		systemInfo.setClientVersionCheck(true);
+		systemInfo.setDeployed("08/20/2013 11:24:40");
+		systemInfo.setEntitlementsToDownload(true);
+		systemInfo.setForcePermanentSession(true);
+		systemInfo.setJidPrepender("sbrw");
+		systemInfo.setLauncherServiceUrl("http://127.0.0.1");
+		systemInfo.setNucleusNamespace("sbrw-live");
+		systemInfo.setNucleusNamespaceWeb("sbr_web");
+		systemInfo.setPersonaCacheTimeout(900);
+		systemInfo.setPortalDomain("soapboxraceworld.com");
+		systemInfo.setPortalStoreFailurePage("soapboxraceworld.com/fail");
+		systemInfo.setPortalTimeOut("6000");
+		systemInfo.setShardName("CORE");
+		GregorianCalendar c = new GregorianCalendar();
+		try {
+			XMLGregorianCalendar date2 = DatatypeFactory.newInstance().newXMLGregorianCalendar(c);
+			systemInfo.setTime(date2);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		systemInfo.setVersion("1599");
+
+		JAXBElement<com.soapboxrace.jaxb.http.SystemInfo> createSystemInfo = new ObjectFactory().createSystemInfo(systemInfo);
+		String marshal = MarshalXML.marshal(createSystemInfo);
+		response.getOutputStream().write(marshal.getBytes());
+	}
+}
