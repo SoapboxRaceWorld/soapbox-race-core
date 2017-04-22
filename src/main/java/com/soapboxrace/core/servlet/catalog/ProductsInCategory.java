ab43a35b48c85fb7c09cfd0c6bb4f3c8eaf1f7ef
Nilzao <nilzao@gmail.com>
2017-04-22 07:07:59 -0400

    product in start store

--- src/main/java/com/soapboxrace/core/servlet/catalog/ProductsInCategory.java
+++ src/main/java/com/soapboxrace/core/servlet/catalog/ProductsInCategory.java
@@ -11,6 +11,7 @@ import javax.xml.namespace.QName;
 
 import com.soapboxrace.core.servlet.GenericServlet;
 import com.soapboxrace.jaxb.http.ArrayOfProductTrans;
+import com.soapboxrace.jaxb.http.ProductTrans;
 import com.soapboxrace.jaxb.util.MarshalXML;
 
 @WebServlet(urlPatterns = { "/Engine.svc/catalog/productsInCategory" })
@@ -24,6 +25,25 @@ public class ProductsInCategory extends GenericServlet {
 	@Override
 	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
 		ArrayOfProductTrans arrayOfProductTrans = new ArrayOfProductTrans();
+		// categoryName=Starting_Cars&clientProductType=PRESETCAR
+		String categoryName = request.getParameter("categoryName");
+		String clientProductType = request.getParameter("clientProductType");
+		if ("Starting_Cars".equals(categoryName) && "PRESETCAR".equals(clientProductType)) {
+			ProductTrans productTrans = new ProductTrans();
+			productTrans.setCurrency("CASH");
+			productTrans.setDurationMinute(0);
+			productTrans.setHash(1133182666);
+			productTrans.setIcon("Black_64x64");
+			productTrans.setLevel(0);
+			productTrans.setPrice(200000.00D);
+			productTrans.setPriority(0);
+			productTrans.setProductId("SRV-SCAR7");
+			productTrans.setProductTitle("BLACK");
+			productTrans.setProductType("PRESETCAR");
+			productTrans.setUseCount(1);
+			arrayOfProductTrans.getProductTrans().add(productTrans);
+		}
+
 		QName qName = new QName("", "ArrayOfProductTrans");
 		JAXBElement<ArrayOfProductTrans> jaxbElement = new JAXBElement<ArrayOfProductTrans>(qName, ArrayOfProductTrans.class, null, arrayOfProductTrans);
 		String marshal = MarshalXML.marshal(jaxbElement, "http://schemas.datacontract.org/2004/07/Victory.DataLayer.Serialization");
