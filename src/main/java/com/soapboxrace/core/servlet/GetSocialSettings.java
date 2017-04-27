8afdd04a3502fab0bfe854c0919c2366ad65210f
Nilzao <nilzao@gmail.com>
2017-04-26 23:09:59 -0400

    servlets

---
+++ src/main/java/com/soapboxrace/core/servlet/GetSocialSettings.java
@@ -0,0 +1,37 @@
+package com.soapboxrace.core.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.xml.bind.JAXBElement;
+
+import com.soapboxrace.jaxb.http.ObjectFactory;
+import com.soapboxrace.jaxb.http.SocialSettings;
+import com.soapboxrace.jaxb.util.MarshalXML;
+
+@WebServlet(urlPatterns = { "/Engine.svc/getsocialsettings" })
+public class GetSocialSettings extends GenericServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 9135516689501603029L;
+
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		SocialSettings socialSettings = new SocialSettings();
+		socialSettings.setAppearOffline(false);
+		socialSettings.setDeclineGroupInvite(0);
+		socialSettings.setDeclineIncommingFriendRequests(false);
+		socialSettings.setDeclinePrivateInvite(0);
+		socialSettings.setHideOfflineFriends(false);
+		socialSettings.setShowNewsOnSignIn(false);
+		socialSettings.setShowOnlyPlayersInSameChatChannel(false);
+		JAXBElement<SocialSettings> createSocialSettings = new ObjectFactory().createSocialSettings(socialSettings);
+		String marshal = MarshalXML.marshal(createSocialSettings);
+		response.getOutputStream().write(marshal.getBytes());
+	}
+}
---
+++ src/main/java/com/soapboxrace/core/servlet/events/AvailableAtLevel.java
@@ -0,0 +1,31 @@
+package com.soapboxrace.core.servlet.events;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.xml.bind.JAXBElement;
+
+import com.soapboxrace.core.servlet.GenericServlet;
+import com.soapboxrace.jaxb.http.EventsPacket;
+import com.soapboxrace.jaxb.http.ObjectFactory;
+import com.soapboxrace.jaxb.util.MarshalXML;
+
+@WebServlet(urlPatterns = { "/Engine.svc/events/availableatlevel" })
+public class AvailableAtLevel extends GenericServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2341802500139316038L;
+
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		EventsPacket eventsPacket = new EventsPacket();
+		JAXBElement<EventsPacket> createEventsPacket = new ObjectFactory().createEventsPacket(eventsPacket);
+		String marshal = MarshalXML.marshal(createEventsPacket);
+		response.getOutputStream().write(marshal.getBytes());
+	}
+}
