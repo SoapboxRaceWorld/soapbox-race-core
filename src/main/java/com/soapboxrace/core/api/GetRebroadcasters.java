da73cd6634c859dbc21ce316e55ad9e5ac5b5b2f
Nilzao <nilzao@gmail.com>
2017-05-16 23:59:08 -0400

    cleaning warnings

--- src/main/java/com/soapboxrace/core/api/GetRebroadcasters.java
+++ src/main/java/com/soapboxrace/core/api/GetRebroadcasters.java
@@ -5,7 +5,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import com.soapboxrace.core.api.util.Secured;
 import com.soapboxrace.jaxb.http.ArrayOfUdpRelayInfo;
 import com.soapboxrace.jaxb.http.UdpRelayInfo;
 
