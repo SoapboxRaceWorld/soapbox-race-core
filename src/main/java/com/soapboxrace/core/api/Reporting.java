1008472bb783c53043129c15f4b3c1358928b08b
Nilzao <nilzao@gmail.com>
2017-05-19 19:42:04 -0400

    sendping bugfix

--- src/main/java/com/soapboxrace/core/api/Reporting.java
+++ src/main/java/com/soapboxrace/core/api/Reporting.java
@@ -4,7 +4,9 @@ import java.io.InputStream;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -38,6 +40,38 @@ public class Reporting {
 	@Path("/SendMultiplayerConnect")
 	@Produces(MediaType.APPLICATION_XML)
 	public String sendMultiplayerConnect() {
+		return "";
+	}
+
+	@GET
+	@Secured
+	@Path("/SendClientPingTime")
+	@Produces(MediaType.APPLICATION_XML)
+	public String sendClientPingTime() {
+		return "";
+	}
+
+	@GET
+	@Path("{path:.*}")
+	@Produces(MediaType.APPLICATION_XML)
+	public String genericEmptyGet(@PathParam("path") String path) {
+		System.out.println("empty GET!!!");
+		return "";
+	}
+
+	@POST
+	@Path("{path:.*}")
+	@Produces(MediaType.APPLICATION_XML)
+	public String genericEmptyPost(@PathParam("path") String path) {
+		System.out.println("empty POST!!!");
+		return "";
+	}
+
+	@PUT
+	@Path("{path:.*}")
+	@Produces(MediaType.APPLICATION_XML)
+	public String genericEmptyPut(@PathParam("path") String path) {
+		System.out.println("empty PUT!!!");
 		return "";
 	}
 }
