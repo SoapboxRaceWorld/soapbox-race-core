9143b94e3aa0faff9d337e6e492e8adf386001a8
Nilzao <nilzao@gmail.com>
2017-05-20 14:28:44 -0400

    admin token tests

--- src/main/java/com/soapboxrace/core/api/CreateTicket.java
+++ src/main/java/com/soapboxrace/core/api/CreateTicket.java
@@ -18,9 +18,13 @@ public class CreateTicket {
 
 	@POST
 	@Produces(MediaType.TEXT_HTML)
-	public String createUser(@FormParam("discordName") String discordName) {
-		InviteTicketEntity createTicket = bo.createTicket(discordName);
-		return "Discord Name: [" + createTicket.getDiscordName() + "] with ticket: <strong>" + createTicket.getTicket() + "</strong>";
+	public String createTicket(@FormParam("discordName") String discordName, @FormParam("token") String token) {
+		if ("123456789".equals(token)) {
+			InviteTicketEntity createTicket = bo.createTicket(discordName);
+			return "Discord Name: [" + createTicket.getDiscordName() + "] with ticket: <strong>" + createTicket.getTicket() + "</strong>";
+		} else {
+			return "ERROR! invalid admin token!";
+		}
 	}
 
 }
--- src/main/webapp/ticket.jsp
+++ src/main/webapp/ticket.jsp
@@ -4,8 +4,9 @@
 	<br />
 	<br />
 	<form action="Engine.svc/CreateTicket" method="post">
-		Discord Name: <input type="text" name="discordName" /><br /> <input
-			type="submit" value="create ticket" />
+		Discord Name: <input type="text" name="discordName" /><br /> Admin
+		Token: <input type="text" name="token" value="123456789" /> <br /> <br />
+		<input type="submit" value="create ticket" />
 	</form>
 	<br />
 	<br />
