7bf0c37ac02ce4825fee5cb13779a97a03d5e40b
Gildarts <alexis19@hotmail.fr>
2017-05-23 16:37:08 +0200

    getPersona by name, dao and entity

--- src/main/java/com/soapboxrace/core/dao/PersonaDAO.java
+++ src/main/java/com/soapboxrace/core/dao/PersonaDAO.java
@@ -1,8 +1,11 @@
 package com.soapboxrace.core.dao;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import com.soapboxrace.core.dao.util.BaseDAO;
 import com.soapboxrace.core.jpa.PersonaEntity;
@@ -17,6 +20,17 @@ public class PersonaDAO extends BaseDAO<PersonaEntity> {
 
 	public PersonaEntity findById(Long id) {
 		return entityManager.find(PersonaEntity.class, id);
+	}
+	
+	public PersonaEntity findByName(String name) {
+		TypedQuery<PersonaEntity> query = entityManager.createNamedQuery("PersonaEntity.findByName", PersonaEntity.class);
+		query.setParameter("name", name);
+		List<PersonaEntity> resultList = query.getResultList();
+		PersonaEntity personaEntity = new PersonaEntity();
+		if(resultList != null && !resultList.isEmpty()){
+			personaEntity = resultList.get(0);
+		}
+		return personaEntity;
 	}
 
 }
--- src/main/java/com/soapboxrace/core/jpa/PersonaEntity.java
+++ src/main/java/com/soapboxrace/core/jpa/PersonaEntity.java
@@ -8,10 +8,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "PERSONA")
+@NamedQueries({ //
+	@NamedQuery(name = "PersonaEntity.findByName", query = "SELECT obj FROM PersonaEntity obj WHERE obj.name = :name") //
+})
 public class PersonaEntity {
 
 	@Id
