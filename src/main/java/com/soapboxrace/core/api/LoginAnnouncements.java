8787045da1b79473b23e68fe764610aecc4274a9
Gildarts <alexis19@hotmail.fr>
2017-05-29 22:05:08 +0200

    DB Info for LoginAnnoucement

--- src/main/java/com/soapboxrace/core/api/LoginAnnouncements.java
+++ src/main/java/com/soapboxrace/core/api/LoginAnnouncements.java
@@ -1,36 +1,25 @@
 package com.soapboxrace.core.api;
 
+import javax.ejb.EJB;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.soapboxrace.core.api.util.Secured;
-import com.soapboxrace.jaxb.http.ArrayOfLoginAnnouncementDefinition;
-import com.soapboxrace.jaxb.http.LoginAnnouncementContext;
-import com.soapboxrace.jaxb.http.LoginAnnouncementDefinition;
-import com.soapboxrace.jaxb.http.LoginAnnouncementType;
+import com.soapboxrace.core.bo.LoginAnnoucementBO;
 import com.soapboxrace.jaxb.http.LoginAnnouncementsDefinition;
 
 @Path("/LoginAnnouncements")
 public class LoginAnnouncements {
+	
+	@EJB
+	private LoginAnnoucementBO bo;
 
 	@GET
 	@Secured
 	@Produces(MediaType.APPLICATION_XML)
 	public LoginAnnouncementsDefinition loginAnnouncements() {
-		LoginAnnouncementsDefinition loginAnnouncementsDefinition = new LoginAnnouncementsDefinition();
-		ArrayOfLoginAnnouncementDefinition arrayOfLoginAnnouncementDefinition = new ArrayOfLoginAnnouncementDefinition();
-		LoginAnnouncementDefinition loginAnnouncementDefinition = new LoginAnnouncementDefinition();
-		loginAnnouncementDefinition.setContext(LoginAnnouncementContext.NOT_APPLICABLE);
-		loginAnnouncementDefinition.setId(1);
-		loginAnnouncementDefinition.setImageChecksum(-1);
-		loginAnnouncementDefinition.setImageUrl("GgMribk.png");
-		loginAnnouncementDefinition.setType(LoginAnnouncementType.EXTERNAL_LINK);
-		loginAnnouncementDefinition.setTarget("https://www.facebook.com/SoapBoxRaceWorld");
-		arrayOfLoginAnnouncementDefinition.getLoginAnnouncementDefinition().add(loginAnnouncementDefinition);
-		loginAnnouncementsDefinition.setAnnouncements(arrayOfLoginAnnouncementDefinition);
-		loginAnnouncementsDefinition.setImagesPath("http://i.imgur.com/");
-		return loginAnnouncementsDefinition;
+		return bo.getLoginAnnouncements();
 	}
 }
