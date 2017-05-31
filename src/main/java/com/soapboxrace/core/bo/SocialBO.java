14459cba2c35866b1028cbe0620fd0c86e635ee8
Gildarts <alexis19@hotmail.fr>
2017-05-31 20:02:34 +0200

    Report Insert DB

--- src/main/java/com/soapboxrace/core/bo/SocialBO.java
+++ src/main/java/com/soapboxrace/core/bo/SocialBO.java
@@ -4,20 +4,24 @@ package com.soapboxrace.core.bo;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
-import com.soapboxrace.core.dao.UserDAO;
+import com.soapboxrace.core.dao.ReportDAO;
+import com.soapboxrace.core.jpa.ReportEntity;
 
 @Stateless
 public class SocialBO {
 
 	@EJB
-	private UserDAO userDao;
-	
-	public String getReportType(Integer petitionType) {
-		return "";
-	}
+	private ReportDAO reportDao;
 	
 	public void sendReport(Long personaId, Long abuserPersonaId, Integer petitionType, String description, Integer customCarID, Integer chatMinutes) {
-		
+		ReportEntity reportEntity = new ReportEntity();
+		reportEntity.setAbuserPersonaId(abuserPersonaId);
+		reportEntity.setChatMinutes(chatMinutes);
+		reportEntity.setCustomCarID(customCarID);
+		reportEntity.setDescription(description);
+		reportEntity.setPersonaId(personaId);
+		reportEntity.setPetitionType(petitionType);
+		reportDao.insert(reportEntity);
 	}
 
 }
