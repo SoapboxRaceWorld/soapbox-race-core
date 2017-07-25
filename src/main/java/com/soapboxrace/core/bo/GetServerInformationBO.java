00a51d6a14443c05ff63c94a7b2331de7c51431a
Nilzao <nilzao@gmail.com>
2017-07-25 06:33:11 -0400

    preparing to finish version

--- src/main/java/com/soapboxrace/core/bo/GetServerInformationBO.java
+++ src/main/java/com/soapboxrace/core/bo/GetServerInformationBO.java
@@ -23,7 +23,7 @@ public class GetServerInformationBO {
 		if (ticketToken != null && !ticketToken.equals("null")) {
 			serverInfoEntity.setRequireTicket(true);
 		}
-		serverInfoEntity.setServerVersion("0.0.7-rc");
+		serverInfoEntity.setServerVersion("0.0.7");
 		return serverInfoEntity;
 	}
 
