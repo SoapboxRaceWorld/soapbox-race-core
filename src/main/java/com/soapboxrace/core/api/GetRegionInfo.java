8c8d7b3be6a6f326a5330c2fa51acbd06f2fc1a6
heyitsleo <coderleo42+leocodes@gmail.com>
2017-10-27 22:14:17 -0400

    aaaaaaaaah

--- src/main/java/com/soapboxrace/core/api/GetRegionInfo.java
+++ src/main/java/com/soapboxrace/core/api/GetRegionInfo.java
@@ -1,9 +1,8 @@
 package com.soapboxrace.core.api;
 
-import com.google.inject.Injector;
+import com.soapboxrace.core.api.util.Secured;
 import com.soapboxrace.jaxb.http.RegionInfo;
 
-import javax.ejb.EJB;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -11,14 +10,10 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/getregioninfo")
 public class GetRegionInfo {
-	@EJB
-	private Injector injector;
-	
 	@GET
-//	@Secured
+	@Secured
 	@Produces(MediaType.APPLICATION_XML)
 	public RegionInfo getRegionInfo() {
-		System.out.println(injector);
 		RegionInfo regionInfo = new RegionInfo();
 		regionInfo.setCountdownProposalInMilliseconds(3000);
 		regionInfo.setDirectConnectTimeoutInMilliseconds(1000);
