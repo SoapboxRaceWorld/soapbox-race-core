a1038893252d91687e35b0231716bccd59b1fea9
heyitsleo <coderleo42+leocodes@gmail.com>
2017-10-29 07:52:20 -0400

    Revert "Revert "Fix for stupidity v4"" well this didn't fix the bad stuff This reverts commit 5b78246de679a411d5194bf2ae7bd73ac1467bc6.

--- src/main/java/com/soapboxrace/core/bo/OnlineUsersBO.java
+++ src/main/java/com/soapboxrace/core/bo/OnlineUsersBO.java
@@ -1,6 +1,9 @@
 package com.soapboxrace.core.bo;
 
+import com.google.inject.Injector;
 import com.soapboxrace.core.dao.OnlineUsersDAO;
+import com.soapboxrace.core.inject.InjectorFactory;
+import com.soapboxrace.core.xmpp.OnlineUsersReporter;
 
 import javax.ejb.EJB;
 import javax.ejb.Schedule;
@@ -14,9 +17,16 @@ public class OnlineUsersBO
 
     @Schedule(minute = "*", hour = "*", persistent = false)
     public void insertNumberOfUsesOnlineNow() {
+        Injector injector = InjectorFactory.getInjector();
+        OnlineUsersReporter reporter = injector.getInstance(OnlineUsersReporter.class);
+        
+        reporter.insertNumberOfUsesOnlineNow(onlineUsersDAO);
     }
 
     public Integer getNumberOfUsersOnlineNow() {
-        return 0;
+        Injector injector = InjectorFactory.getInjector();
+        OnlineUsersReporter reporter = injector.getInstance(OnlineUsersReporter.class);
+
+        return reporter.getNumberOfUsersOnlineNow(onlineUsersDAO);
     }
 }
