4a16177667d2489b1f3f4844c49501c470576acd
Nilzao <nilzao@gmail.com>
2018-02-16 22:54:36 -0300

    cleaning warnings

--- src/main/java/com/soapboxrace/core/api/Reporting.java
+++ src/main/java/com/soapboxrace/core/api/Reporting.java
@@ -51,22 +51,6 @@ public class Reporting {
 		return "";
 	}
 
-	@GET
-	@Path("{path:.*}")
-	@Produces(MediaType.APPLICATION_XML)
-	public String genericEmptyGet(@PathParam("path") String path) {
-		System.out.println("empty GET!!!");
-		return "";
-	}
-
-	@POST
-	@Path("{path:.*}")
-	@Produces(MediaType.APPLICATION_XML)
-	public String genericEmptyPost(@PathParam("path") String path) {
-		System.out.println("empty POST!!!");
-		return "";
-	}
-
 	@PUT
 	@Path("{path:.*}")
 	@Produces(MediaType.APPLICATION_XML)
