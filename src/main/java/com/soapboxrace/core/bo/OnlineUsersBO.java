ea1158fc9ee2a645f984b12e8deda4f3d9a8a4df
Nilzao <nilzao@gmail.com>
2018-02-16 23:07:00 -0300

    online users

--- src/main/java/com/soapboxrace/core/bo/OnlineUsersBO.java
+++ src/main/java/com/soapboxrace/core/bo/OnlineUsersBO.java
@@ -1,30 +1,43 @@
 package com.soapboxrace.core.bo;
 
+import java.util.Date;
+
 import javax.ejb.EJB;
 import javax.ejb.Schedule;
 import javax.ejb.Stateless;
 
 import com.soapboxrace.core.dao.OnlineUsersDAO;
+import com.soapboxrace.core.jpa.OnlineUsersEntity;
+import com.soapboxrace.core.xmpp.OpenFireRestApiCli;
 
 @Stateless
 public class OnlineUsersBO {
+
 	@EJB
 	private OnlineUsersDAO onlineUsersDAO;
 
-	@Schedule(minute = "*", hour = "*", persistent = false)
-	public void insertNumberOfUsesOnlineNow() {
-		// Injector injector = InjectorFactory.getInjector();
-		// OnlineUsersReporter reporter =
-		// injector.getInstance(OnlineUsersReporter.class);
-		//
-		// reporter.insertNumberOfUsesOnlineNow(onlineUsersDAO);
+	@EJB
+	OpenFireRestApiCli openFireRestApiCli;
+
+	public int getNumberOfUsersOnlineNow() {
+		Date lastMinutes = getLastMinutes(1);
+		OnlineUsersEntity onlineUsersEntity = onlineUsersDAO.findByTime(lastMinutes);
+		return onlineUsersEntity != null ? onlineUsersEntity.getNumberOfUsers() : 0;
 	}
 
-	public Integer getNumberOfUsersOnlineNow() {
-		// Injector injector = InjectorFactory.getInjector();
-		// OnlineUsersReporter reporter =
-		// injector.getInstance(OnlineUsersReporter.class);
-		// return reporter.getNumberOfUsersOnlineNow(onlineUsersDAO);
-		return 0;
+	@Schedule(minute = "*", hour = "*", persistent = false)
+	public void insertNumberOfUsesOnlineNow() {
+		Long timeLong = new Date().getTime() / 1000L;
+		OnlineUsersEntity onlineUsersEntity = new OnlineUsersEntity();
+		onlineUsersEntity.setNumberOfUsers(openFireRestApiCli.getTotalOnlineUsers());
+		onlineUsersEntity.setTimeRecord(timeLong.intValue());
+		onlineUsersDAO.insert(onlineUsersEntity);
+	}
+
+	private Date getLastMinutes(int minutes) {
+		long time = new Date().getTime();
+		time = time - (minutes * 90000);
+
+		return new Date(time);
 	}
 }
