2f859cc84e8760e5acdd1bcbc54249c1e4e10b74
Nilzao <nilzao@gmail.com>
2018-03-10 11:37:41 -0400

    set detected xmpp ip if set to localhost

--- src/main/java/com/soapboxrace/core/api/Session.java
+++ src/main/java/com/soapboxrace/core/api/Session.java
@@ -1,10 +1,14 @@
 package com.soapboxrace.core.api;
 
+import java.net.URI;
+
 import javax.ejb.EJB;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 
 import com.soapboxrace.core.api.util.Secured;
 import com.soapboxrace.core.bo.ParameterBO;
@@ -13,6 +17,9 @@ import com.soapboxrace.jaxb.http.ChatServer;
 
 @Path("/Session")
 public class Session {
+
+	@Context
+	UriInfo uri;
 
 	@EJB
 	private SessionBO bo;
@@ -27,6 +34,11 @@ public class Session {
 	public ChatServer getChatInfo() {
 		ChatServer chatServer = new ChatServer();
 		chatServer.setIp(parameterBO.getStrParam("XMPP_IP"));
+		if ("127.0.0.1".equals(parameterBO.getStrParam("XMPP_IP"))) {
+			URI myUri = uri.getBaseUri();
+			String host = myUri.getHost();
+			chatServer.setIp(host);
+		}
 		chatServer.setPort(parameterBO.getIntParam("XMPP_PORT"));
 		chatServer.setPrefix("sbrw");
 		chatServer.setRooms(bo.getAllChatRoom());
