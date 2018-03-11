111efd0e9d17bc63992c0e3c19a94cf5247ae1d3
Nilzao <nilzao@gmail.com>
2018-03-11 18:07:55 -0400

    xmpp ping nullpointer fix

--- src/main/java/com/soapboxrace/core/xmpp/standard/StandardOpenFireTalk.java
+++ src/main/java/com/soapboxrace/core/xmpp/standard/StandardOpenFireTalk.java
@@ -8,20 +8,17 @@ import com.soapboxrace.jaxb.util.UnmarshalXML;
 import com.soapboxrace.jaxb.xmpp.XMPP_IQPingType;
 import com.soapboxrace.jaxb.xmpp.XMPP_IQPongType;
 
-public class StandardOpenFireTalk extends BaseOpenFireTalk
-{
-    public StandardOpenFireTalk(Socket socket)
-    {
-        super(socket);
-    }
+public class StandardOpenFireTalk extends BaseOpenFireTalk {
+	public StandardOpenFireTalk(Socket socket) {
+		super(socket);
+	}
 
-    @Override
-    public void handleMessage(String msg)
-    {
-        if (msg.contains("<ping xmlns=\"urn:xmpp:ping\"/>"))
-        {
-            XMPP_IQPingType openfirePing = UnmarshalXML.unMarshal(msg, XMPP_IQPingType.class);
-            write(MarshalXML.marshal(new XMPP_IQPongType(openfirePing.getId())));
-        }
-    }
+	@Override
+	public void handleMessage(String msg) {
+		if (msg.contains("<ping xmlns=\"urn:xmpp:ping\"/>")) {
+			String hostAddress = socket.getInetAddress().getHostAddress();
+			XMPP_IQPingType openfirePing = UnmarshalXML.unMarshal(msg, XMPP_IQPingType.class);
+			write(MarshalXML.marshal(new XMPP_IQPongType(openfirePing.getId(), hostAddress)));
+		}
+	}
 }
--- src/main/java/com/soapboxrace/jaxb/xmpp/XMPP_IQPongType.java
+++ src/main/java/com/soapboxrace/jaxb/xmpp/XMPP_IQPongType.java
@@ -1,23 +1,15 @@
 package com.soapboxrace.jaxb.xmpp;
 
-import javax.ejb.EJB;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
-
-import com.soapboxrace.core.bo.ParameterBO;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "XMPP_IQPongType", propOrder = { "from", "to", "id", "type" })
 @XmlRootElement(name = "iq")
 public class XMPP_IQPongType {
-
-	@EJB
-	@XmlTransient
-	private ParameterBO parameterBO;
 
 	@XmlAttribute(name = "from")
 	private String from;
@@ -31,9 +23,9 @@ public class XMPP_IQPongType {
 	public XMPP_IQPongType() {
 	}
 
-	public XMPP_IQPongType(String id) {
-		from = String.format("sbrw.engine.engine@%s/EA_Chat", parameterBO.getStrParam("XMPP_IP"));
-		to = parameterBO.getStrParam("XMPP_IP");
+	public XMPP_IQPongType(String id, String xmppIp) {
+		from = String.format("sbrw.engine.engine@%s/EA_Chat", xmppIp);
+		to = xmppIp;
 		this.id = id;
 	}
 
