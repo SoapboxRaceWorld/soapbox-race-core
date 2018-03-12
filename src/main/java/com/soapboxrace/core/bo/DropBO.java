4b7fa0a45ecce7257396b7e7bc8a224e83536c31
Nilzao <nilzao@gmail.com>
2018-03-12 05:43:19 -0400

    dropBO testing

--- src/main/java/com/soapboxrace/core/bo/DropBO.java
+++ src/main/java/com/soapboxrace/core/bo/DropBO.java
@@ -3,13 +3,15 @@ package com.soapboxrace.core.bo;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
 import com.soapboxrace.core.dao.InventoryDAO;
 import com.soapboxrace.core.dao.InventoryItemDAO;
 import com.soapboxrace.core.dao.LevelRepDAO;
 import com.soapboxrace.core.dao.PersonaDAO;
 import com.soapboxrace.core.dao.ProductDAO;
-import com.soapboxrace.core.jpa.PersonaEntity;
-import com.soapboxrace.jaxb.http.ArrayOfLuckyDrawItem;
+import com.soapboxrace.core.jpa.ProductEntity;
+import com.soapboxrace.jaxb.http.LuckyDrawItem;
 
 @Stateless
 public class DropBO {
@@ -28,7 +30,21 @@ public class DropBO {
 	@EJB
 	private ProductDAO productDao;
 
-	public ArrayOfLuckyDrawItem dropRaceItem(PersonaEntity personaEntity, Integer rank) {
-		return null;
+	public LuckyDrawItem getRandomItem() {
+		ProductEntity productEntity = productDao.getRandomDrop();
+		LuckyDrawItem luckyDrawItem = new LuckyDrawItem();
+		luckyDrawItem.setDescription(productEntity.getProductTitle());
+		luckyDrawItem.setHash(productEntity.getHash());
+		luckyDrawItem.setIcon(productEntity.getIcon());
+		Integer count = 1;
+		if ("POWERUP".equals(productEntity.getProductType())) {
+			count = 15;
+		}
+		luckyDrawItem.setRemainingUseCount(count);
+		luckyDrawItem.setResellPrice(productEntity.getResalePrice());
+		luckyDrawItem.setVirtualItem(DigestUtils.md5Hex(productEntity.getHash().toString()));
+		luckyDrawItem.setVirtualItemType(productEntity.getProductType());
+		luckyDrawItem.setWasSold(false);
+		return luckyDrawItem;
 	}
 }
