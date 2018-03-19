fdd1245c8a58b9560ddf8890f94a729304af8386
heyitsleo <coderleo42+leocodes@gmail.com>
2018-03-19 16:38:23 -0400

    Initial achievements commit

--- src/main/java/com/soapboxrace/core/api/AddFriendRequest.java
+++ src/main/java/com/soapboxrace/core/api/AddFriendRequest.java
@@ -17,42 +17,10 @@ import com.soapboxrace.jaxb.xmpp.XMPP_FriendPersonaType;
 
 @Path("/addfriendrequest")
 public class AddFriendRequest {
-	@EJB
-	private TokenSessionBO sessionBO;
-	
-	@EJB
-	private PersonaDAO personaDAO;
-
-	@EJB
-	private OpenFireSoapBoxCli openFireSoapBoxCli;
-	
 	@GET
 	@Secured
 	@Produces(MediaType.APPLICATION_XML)
-	public String addFriendRequest(@HeaderParam("securityToken") String securityToken, @QueryParam("displayName") String displayName) {
-		long activePersonaId = sessionBO.getActivePersonaId(securityToken);
-		PersonaEntity active = personaDAO.findById(activePersonaId);
-		PersonaEntity target = personaDAO.findByName(displayName);
-		
-		if (target == null) 
-			return "";
-
-		openFireSoapBoxCli.send(XmppChat.createSystemMessage("Sent friend request!"), activePersonaId);
-		openFireSoapBoxCli.send(XmppChat.createSystemMessage(String.format("You received a friend request from %s!", active.getName())), target.getPersonaId());
-		
-		{
-			XMPP_FriendPersonaType friendPersona = new XMPP_FriendPersonaType();
-			friendPersona.setIconIndex(active.getIconIndex());
-			friendPersona.setLevel(active.getLevel());
-			friendPersona.setName(active.getName());
-			friendPersona.setOriginalName(active.getName());
-			friendPersona.setPersonaId(activePersonaId);
-			friendPersona.setPresence(3);
-			friendPersona.setUserId(active.getUser().getId());
-			
-			openFireSoapBoxCli.send(MarshalXML.marshal(friendPersona), target.getPersonaId());
-		}
-		
+	public String addFriendRequest() {
 		return "";
 	}
 }
