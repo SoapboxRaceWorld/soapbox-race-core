4be6bd6e72a1df37f5370b3f77f27cf731aa6a8f
Nilzao <nilzao@gmail.com>
2018-03-23 22:22:41 -0400

    temporary sql to fix rewards calc

---
+++ sql/99-tmpfix-rewards.sql
@@ -0,0 +1,11 @@
+UPDATE SOAPBOX.EVENT SET 
+baseCashReward='1000', baseRepReward='1000', 
+finalCashRewardMultiplier='0.1', finalRepRewardMultiplier='0.1', 
+legitTime='20000', levelCashRewardMultiplier='0.1', levelRepRewardMultiplier='0.1', 
+minTopSpeedTrigger='50', perfectStartCashMultiplier='0.1', perfectStartRepMultiplier='0.1', 
+rank1CashMultiplier='0.1', rank1RepMultiplier='0.1', rank2CashMultiplier='0.1', 
+rank2RepMultiplier='0.1', rank3CashMultiplier='0.1', rank3RepMultiplier='0.1', 
+rank4CashMultiplier='0.1', rank4RepMultiplier='0.1', rank5CashMultiplier='0.1', 
+rank5RepMultiplier='0.1', rank6CashMultiplier='0.1', rank6RepMultiplier='0.1', 
+rank7CashMultiplier='0.1', rank7RepMultiplier='0.1', rank8CashMultiplier='0.1', 
+rank8RepMultiplier='0.1', topSpeedCashMultiplier='0.1', topSpeedRepMultiplier='0.1' ;
