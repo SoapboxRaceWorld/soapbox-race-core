49f4844099a2fab61c334c5c812a40e91fcca26f
LeoCodes21 <coderleo42+leocodes@gmail.com>
2018-07-01 20:17:48 -0400

    Inverted arguments #3

--- src/main/java/com/soapboxrace/core/api/RemoveFriend.java
+++ src/main/java/com/soapboxrace/core/api/RemoveFriend.java
@@ -45,8 +45,10 @@ public class RemoveFriend
             return "";
         }
 
-        FriendEntity friendEntity = friendDAO.findBySenderAndRecipient(friend.getPersonaId(), active.getUser().getId());
-        FriendEntity friendEntity2 = friendDAO.findBySenderAndRecipient(active.getPersonaId(), friend.getUser().getId());
+        FriendEntity friendEntity = friendDAO.findBySenderAndRecipient(active.getUser().getId(), friend.getPersonaId());
+        FriendEntity friendEntity2 = friendDAO.findBySenderAndRecipient(friend.getUser().getId(), active.getPersonaId());
+//        FriendEntity friendEntity = friendDAO.findBySenderAndRecipient(friend.getPersonaId(), active.getUser().getId());
+//        FriendEntity friendEntity2 = friendDAO.findBySenderAndRecipient(active.getPersonaId(), friend.getUser().getId());
         
         if (friendEntity == null || friendEntity2 == null) {
             System.err.println("Hit the other bad spot in removefriend! Something's up...");
