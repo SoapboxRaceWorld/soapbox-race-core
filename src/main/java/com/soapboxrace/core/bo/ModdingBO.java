bfbbd3484e48fb711b4aceffe0d23d186ad8e007
LeoCodes21 <coderleo42+leocodes@gmail.com>
2019-08-31 17:46:21 -0400

    add 'features' to ModInfoVO

--- src/main/java/com/soapboxrace/core/bo/ModdingBO.java
+++ src/main/java/com/soapboxrace/core/bo/ModdingBO.java
@@ -4,6 +4,9 @@ import com.soapboxrace.core.vo.ModInfoVO;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Stateless
 public class ModdingBO {
@@ -16,9 +19,19 @@ public class ModdingBO {
             return null;
         }
 
+        String moddingBasePath = parameterBO.getStrParam("MODDING_BASE_PATH");
         ModInfoVO modInfoVO = new ModInfoVO();
         modInfoVO.setServerID(parameterBO.getStrParam("MODDING_SERVER_ID"));
-        modInfoVO.setBasePath(parameterBO.getStrParam("MODDING_BASE_PATH"));
+        modInfoVO.setBasePath(moddingBasePath == null ? "" : moddingBasePath);
+
+        List<String> features = new ArrayList<>();
+
+        String moddingFeatures = parameterBO.getStrParam("MODDING_FEATURES");
+
+        if (moddingFeatures != null && !moddingFeatures.isEmpty()) {
+            features.addAll(Arrays.asList(moddingFeatures.split(";")));
+        }
+        modInfoVO.setFeatures(features);
 
         return modInfoVO;
     }
--- src/main/java/com/soapboxrace/core/vo/ModInfoVO.java
+++ src/main/java/com/soapboxrace/core/vo/ModInfoVO.java
@@ -1,10 +1,14 @@
 package com.soapboxrace.core.vo;
 
+import java.util.List;
+
 public class ModInfoVO {
 
     private String basePath;
 
     private String serverID;
+
+    private List<String> features;
 
     public String getBasePath() {
         return basePath;
@@ -20,5 +24,13 @@ public class ModInfoVO {
 
     public void setServerID(String serverID) {
         this.serverID = serverID;
+    }
+
+    public List<String> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(List<String> features) {
+        this.features = features;
     }
 }
