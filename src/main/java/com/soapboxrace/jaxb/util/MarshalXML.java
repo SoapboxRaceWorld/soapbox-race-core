85b18f10708bf0ac896a7accf9a65df2909177de
LeoCodes21 <coderleo42+leocodes@gmail.com>
2019-11-26 20:41:49 -0500

    add copyright notice

--- src/main/java/com/soapboxrace/jaxb/util/MarshalXML.java
+++ src/main/java/com/soapboxrace/jaxb/util/MarshalXML.java
@@ -1,3 +1,9 @@
+/*
+ * This file is part of the Soapbox Race World core source code.
+ * If you use any of this code for third-party purposes, please provide attribution.
+ * Copyright (c) 2019.
+ */
+
 package com.soapboxrace.jaxb.util;
 
 import java.io.StringWriter;
