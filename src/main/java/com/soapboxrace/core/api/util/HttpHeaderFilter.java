b81a39cec391815a503278cc33083c8a6a3d8374
leo <coderleo42+leocodes@gmail.com>
2020-02-01 20:56:37 -0500

    remove print from HttpHeaderFilter

--- src/main/java/com/soapboxrace/core/api/util/HttpHeaderFilter.java
+++ src/main/java/com/soapboxrace/core/api/util/HttpHeaderFilter.java
@@ -10,14 +10,12 @@ import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
-import java.io.IOException;
 
 @Provider
 public class HttpHeaderFilter implements ContainerResponseFilter {
 
     @Override
-    public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
-        System.out.println("[" + request.getMethod() + "] " + request.getUriInfo().getPath() + " " + request.getUriInfo().getRequestUri().getQuery());
+    public void filter(ContainerRequestContext request, ContainerResponseContext response) {
         response.getHeaders().add("Connection", "close");
     }
 }
