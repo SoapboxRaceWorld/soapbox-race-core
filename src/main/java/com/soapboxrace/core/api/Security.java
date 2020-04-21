66ab743189bf09aaf3dafc62b917d63aaae52ec0
leo <coderleo42+leocodes@gmail.com>
2020-04-21 16:31:55 -0400

    [experimental] send FraudConfig with gameFileFreq set to -1 to effectively disable file checks

--- src/main/java/com/soapboxrace/core/api/Security.java
+++ src/main/java/com/soapboxrace/core/api/Security.java
@@ -23,7 +23,8 @@ public class Security {
     public FraudConfig fraudConfig(@HeaderParam("userId") Long userId) {
         FraudConfig fraudConfig = new FraudConfig();
         fraudConfig.setEnabledBitField(12);
-        fraudConfig.setGameFileFreq(1000000);
+//        fraudConfig.setGameFileFreq(1000000);
+        fraudConfig.setGameFileFreq(-1);
         fraudConfig.setModuleFreq(360000);
         fraudConfig.setStartUpFreq(1000000);
         fraudConfig.setUserID(userId);
