644ae45afbb85e1dd99f6af4cfef23f91909c011
leo <coderleo42+leocodes@gmail.com>
2020-04-24 09:52:34 -0400

    EventResultBO: clear ModifiedFiles flag from hacksDetected

--- src/main/java/com/soapboxrace/core/bo/EventResultBO.java
+++ src/main/java/com/soapboxrace/core/bo/EventResultBO.java
@@ -30,22 +30,26 @@ public class EventResultBO {
     public PursuitEventResult handlePursitEnd(EventSessionEntity eventSessionEntity, Long activePersonaId,
                                               PursuitArbitrationPacket pursuitArbitrationPacket,
                                               Boolean isBusted) {
+        pursuitArbitrationPacket.setHacksDetected(pursuitArbitrationPacket.getHacksDetected() & ~32);
         return eventResultPursuitBO.handlePursuitEnd(eventSessionEntity, activePersonaId, pursuitArbitrationPacket,
                 isBusted);
     }
 
     public RouteEventResult handleRaceEnd(EventSessionEntity eventSessionEntity, Long activePersonaId,
                                           RouteArbitrationPacket routeArbitrationPacket) {
+        routeArbitrationPacket.setHacksDetected(routeArbitrationPacket.getHacksDetected() & ~32);
         return eventResultRouteBO.handleRaceEnd(eventSessionEntity, activePersonaId, routeArbitrationPacket);
     }
 
     public DragEventResult handleDragEnd(EventSessionEntity eventSessionEntity, Long activePersonaId,
                                          DragArbitrationPacket dragArbitrationPacket) {
+        dragArbitrationPacket.setHacksDetected(dragArbitrationPacket.getHacksDetected() & ~32);
         return eventResultDragBO.handleDragEnd(eventSessionEntity, activePersonaId, dragArbitrationPacket);
     }
 
     public TeamEscapeEventResult handleTeamEscapeEnd(EventSessionEntity eventSessionEntity, Long activePersonaId,
                                                      TeamEscapeArbitrationPacket teamEscapeArbitrationPacket) {
+        teamEscapeArbitrationPacket.setHacksDetected(teamEscapeArbitrationPacket.getHacksDetected() & ~32);
         return eventResultTeamEscapeBO.handleTeamEscapeEnd(eventSessionEntity, activePersonaId,
                 teamEscapeArbitrationPacket);
     }
