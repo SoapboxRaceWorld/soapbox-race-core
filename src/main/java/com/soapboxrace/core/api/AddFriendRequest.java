2eb0116e99dcade5ce262cb51e6240ade70071a3
DriFtyZ <petrosdrift@gmail.com>
2020-05-29 23:22:44 +1000

    AddFriendRequest.java: Checks for admin commands
    Admin commands can be now passed through the "add friend" window for quicker access

--- src/main/java/com/soapboxrace/core/api/AddFriendRequest.java
+++ src/main/java/com/soapboxrace/core/api/AddFriendRequest.java
@@ -7,7 +7,9 @@
 package com.soapboxrace.core.api;
 
 import com.soapboxrace.core.api.util.Secured;
+import com.soapboxrace.core.dao.PersonaDAO;
 import com.soapboxrace.core.bo.SocialRelationshipBO;
+import com.soapboxrace.core.bo.AdminBO;
 import com.soapboxrace.core.bo.TokenSessionBO;
 
 import javax.ejb.EJB;
@@ -18,7 +20,13 @@ import javax.ws.rs.core.Response;
 @Path("/addfriendrequest")
 public class AddFriendRequest {
     @EJB
+    private AdminBO adminBO;
+
+    @EJB
     private TokenSessionBO tokenSessionBO;
+
+    @EJB
+    private PersonaDAO personaDAO;
 
     @EJB
     private SocialRelationshipBO socialRelationshipBO;
@@ -29,6 +37,16 @@ public class AddFriendRequest {
     public Response addFriendRequest(@HeaderParam("securityToken") String securityToken,
                                      @QueryParam("displayName") String displayName) {
         Long activePersonaId = tokenSessionBO.getActivePersonaId(securityToken);
+
+        if (tokenSessionBO.isAdmin(securityToken) && displayName.startsWith("/")) {
+            String[] abuseCase = displayName.split(" ");
+            Long abuserId = personaDAO.findByName(abuseCase[1].trim()).getPersonaId();
+            if (abuserId != null) {
+                adminBO.sendCommand(tokenSessionBO.getActivePersonaId(securityToken), abuserId, displayName);
+            } else {
+                // System.out.println("Command: " + abuseCase[0] + " Name: " + abuseCase[1] + " (NOT FOUND)");
+            }
+        }
         return Response.ok().entity(socialRelationshipBO.addFriend(activePersonaId, displayName)).build();
     }
 }
