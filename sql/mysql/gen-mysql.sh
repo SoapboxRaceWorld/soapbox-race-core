208f8c7e0599125ce5a37ef89ea6acdf85bf7190
leo <coderleo42+leocodes@gmail.com>
2020-05-30 18:32:20 -0400

    some cleanup

--- sql/mysql/gen-mysql.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SCRIPT_NAME=full-script.sql
-cat 00-mysql-structure.sql > $SCRIPT_NAME
-cat 01-mysql-openfire.sql >> $SCRIPT_NAME
-cat ../01*.sql >> $SCRIPT_NAME
-cat ../02*.sql >> $SCRIPT_NAME
