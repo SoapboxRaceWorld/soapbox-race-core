9acb2639aeda004ba9382a258588a5b5170586d3
leo <coderleo42+leocodes@gmail.com>
2020-05-30 18:15:37 -0400

    powerups/activated: call EventPowerupBO.createPowerupRecord

--- src/main/java/com/soapboxrace/core/api/Powerups.java
+++ src/main/java/com/soapboxrace/core/api/Powerups.java
@@ -41,6 +41,9 @@ public class Powerups {
     @EJB
     private AchievementBO achievementBO;
 
+    @EJB
+    private EventPowerupBO eventPowerupBO;
+
     @POST
     @Secured
     @Path("/activated/{powerupHash}")
@@ -69,6 +72,10 @@ public class Powerups {
             AchievementTransaction transaction = achievementBO.createTransaction(activePersonaId);
             transaction.add("INVENTORY", Map.of("persona", personaEntity, "ctx", new AchievementInventoryContext(inventoryItemEntity, AchievementInventoryContext.Event.QUANTITY_DECREASED)));
             achievementBO.commitTransaction(personaEntity, transaction);
+        }
+
+        if (eventSessionId != 0L) {
+            eventPowerupBO.createPowerupRecord(eventSessionId, activePersonaId, powerupHash);
         }
 
         return "";
