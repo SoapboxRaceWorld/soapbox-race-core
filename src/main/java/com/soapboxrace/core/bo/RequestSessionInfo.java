22973ac0b2d3bcbd9a3fee4eda9d79c0b8f3fa11
leo <coderleo42+leocodes@gmail.com>
2020-08-27 13:26:07 -0400

    add TokenSessionDAO as injected bean in RequestSessionInfo

--- src/main/java/com/soapboxrace/core/bo/RequestSessionInfo.java
+++ src/main/java/com/soapboxrace/core/bo/RequestSessionInfo.java
@@ -1,14 +1,19 @@
 package com.soapboxrace.core.bo;
 
+import com.soapboxrace.core.dao.TokenSessionDAO;
 import com.soapboxrace.core.jpa.TokenSessionEntity;
 import com.soapboxrace.core.jpa.UserEntity;
 
+import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 
 @RequestScoped
 public class RequestSessionInfo {
 
     private TokenSessionEntity tokenSessionEntity;
+
+    @EJB
+    private TokenSessionDAO tokenSessionDAO;
 
     public TokenSessionEntity getTokenSessionEntity() {
         return tokenSessionEntity;
