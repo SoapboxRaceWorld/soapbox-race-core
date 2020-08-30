11ea495a535a858e9c308513752d0f0bc6b22d1e
leo <coderleo42+leocodes@gmail.com>
2020-08-30 14:24:33 -0400

    Add a LoggerProducer class that exposes SLF4J logger objects to beans

--- src/main/java/com/soapboxrace/core/bo/ErrorReportingBO.java
+++ src/main/java/com/soapboxrace/core/bo/ErrorReportingBO.java
@@ -2,37 +2,38 @@ package com.soapboxrace.core.bo;
 
 import io.sentry.SentryClient;
 import io.sentry.SentryClientFactory;
+import org.slf4j.Logger;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.*;
+import javax.inject.Inject;
 
 @Startup
 @Singleton
 public class ErrorReportingBO {
 
-    private SentryClient sentryClient;
-
     @EJB
     private ParameterBO parameterBO;
 
-    private boolean enabled;
+    @Inject
+    private Logger logger;
+
+    private SentryClient sentryClient;
 
     @PostConstruct
     public void init() {
-        System.out.println("ErrorReportingBO is starting");
-        this.enabled = parameterBO.getBoolParam("ENABLE_SENTRY_REPORTING");
-
-        if (this.enabled) {
+        if (parameterBO.getBoolParam("ENABLE_SENTRY_REPORTING")) {
             this.sentryClient = SentryClientFactory.sentryClient(parameterBO.getStrParam("SENTRY_DSN"));
+            this.logger.info("Sentry support is enabled");
         }
 
-        System.out.println("ErrorReportingBO has started");
+        this.logger.info("Initialized error reporting system");
     }
 
     @Asynchronous
     @Lock(LockType.READ)
     public void sendException(Exception exception) {
-        if (this.enabled) {
+        if (this.sentryClient != null) {
             this.sentryClient.sendException(exception);
         }
     }
---
+++ src/main/java/com/soapboxrace/core/bo/util/LoggerProducer.java
@@ -0,0 +1,19 @@
+package com.soapboxrace.core.bo.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+@Named
+@Singleton
+public class LoggerProducer {
+
+    @Produces
+    public Logger produceLogger(InjectionPoint injectionPoint) {
+        return LoggerFactory.getLogger(injectionPoint.getBean().getBeanClass());
+    }
+}
