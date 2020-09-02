ef6742b1fe04c1b412881269fbe75a5931f39a9b
leo <coderleo42+leocodes@gmail.com>
2020-09-02 16:32:05 -0400

    Implement login announcement caching

--- src/main/java/com/soapboxrace/core/bo/LoginAnnouncementBO.java
+++ src/main/java/com/soapboxrace/core/bo/LoginAnnouncementBO.java
@@ -11,10 +11,15 @@ import com.soapboxrace.core.jpa.LoginAnnouncementEntity;
 import com.soapboxrace.jaxb.http.*;
 
 import javax.ejb.EJB;
-import javax.ejb.Stateless;
+import javax.ejb.Lock;
+import javax.ejb.LockType;
+import javax.ejb.Singleton;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
-@Stateless
+@Singleton
+@Lock(LockType.READ)
 public class LoginAnnouncementBO {
 
     @EJB
@@ -23,25 +28,33 @@ public class LoginAnnouncementBO {
     @EJB
     private ParameterBO parameterBO;
 
+    private final ConcurrentMap<String, LoginAnnouncementsDefinition> announcementsCache = new ConcurrentHashMap<>();
+
+    public void resetCachedAnnouncements() {
+        announcementsCache.clear();
+    }
+
     public LoginAnnouncementsDefinition getLoginAnnouncements(String language) {
-        ArrayOfLoginAnnouncementDefinition arrayOfLoginAnnouncementDefinition =
-                new ArrayOfLoginAnnouncementDefinition();
+        return announcementsCache.computeIfAbsent(language, key -> {
+            LoginAnnouncementsDefinition loginAnnouncementsDefinition = new LoginAnnouncementsDefinition();
+            ArrayOfLoginAnnouncementDefinition arrayOfLoginAnnouncementDefinition =
+                    new ArrayOfLoginAnnouncementDefinition();
 
-        List<LoginAnnouncementEntity> listOfLoginAnnoucement = loginAnnouncementDao.findAllByLanguage(language);
-        for (LoginAnnouncementEntity entity : listOfLoginAnnoucement) {
-            LoginAnnouncementDefinition loginAnnouncementDefinition = new LoginAnnouncementDefinition();
-            loginAnnouncementDefinition.setContext(LoginAnnouncementContext.fromValue(entity.getContext()));
-            loginAnnouncementDefinition.setId(entity.getId());
-            loginAnnouncementDefinition.setImageChecksum(-1);
-            loginAnnouncementDefinition.setImageUrl(entity.getImageUrl());
-            loginAnnouncementDefinition.setType(LoginAnnouncementType.fromValue(entity.getType()));
-            loginAnnouncementDefinition.setTarget(entity.getTarget());
-            arrayOfLoginAnnouncementDefinition.getLoginAnnouncementDefinition().add(loginAnnouncementDefinition);
-        }
+            List<LoginAnnouncementEntity> listOfLoginAnnoucement = loginAnnouncementDao.findAllByLanguage(key);
+            for (LoginAnnouncementEntity entity : listOfLoginAnnoucement) {
+                LoginAnnouncementDefinition loginAnnouncementDefinition = new LoginAnnouncementDefinition();
+                loginAnnouncementDefinition.setContext(LoginAnnouncementContext.fromValue(entity.getContext()));
+                loginAnnouncementDefinition.setId(entity.getId());
+                loginAnnouncementDefinition.setImageChecksum(-1);
+                loginAnnouncementDefinition.setImageUrl(entity.getImageUrl());
+                loginAnnouncementDefinition.setType(LoginAnnouncementType.fromValue(entity.getType()));
+                loginAnnouncementDefinition.setTarget(entity.getTarget());
+                arrayOfLoginAnnouncementDefinition.getLoginAnnouncementDefinition().add(loginAnnouncementDefinition);
+            }
 
-        LoginAnnouncementsDefinition loginAnnouncementsDefinition = new LoginAnnouncementsDefinition();
-        loginAnnouncementsDefinition.setAnnouncements(arrayOfLoginAnnouncementDefinition);
-        loginAnnouncementsDefinition.setImagesPath(parameterBO.getStrParam("ANNOUNCEMENT_DOMAIN"));
-        return loginAnnouncementsDefinition;
+            loginAnnouncementsDefinition.setAnnouncements(arrayOfLoginAnnouncementDefinition);
+            loginAnnouncementsDefinition.setImagesPath(parameterBO.getStrParam("ANNOUNCEMENT_DOMAIN"));
+            return loginAnnouncementsDefinition;
+        });
     }
 }
