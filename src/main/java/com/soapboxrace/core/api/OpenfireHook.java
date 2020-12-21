5f8fe25092d93f5e2c0310c53f2fee6f581d8dc4
Milan Suwała <72949798+metonator@users.noreply.github.com>
2020-12-21 19:35:15 +0100

    Update OpenfireHook.java

--- src/main/java/com/soapboxrace/core/api/OpenfireHook.java
+++ src/main/java/com/soapboxrace/core/api/OpenfireHook.java
@@ -38,7 +38,6 @@ public class OpenfireHook {
             adminBO.sendChatCommand(persona, command, personaEntity.getName());
         }
         
-        //Just to force actions...
         return Response.noContent().build();
     }
 }
